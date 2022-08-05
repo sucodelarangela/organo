3d66e31a43565f5ed02cf83690c814b6fa89dc71
Angela Caldas <aes.caldas@gmail.com>
2022-08-04 21:07:47 -0300

    :sparkles: banner component

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,15 @@
+// styles
+import './Banner.css';
+
+const Banner = () => {
+    return (
+        <header className='banner'>
+            <img src="/images/banner.png" alt="banner principal da página do Organo" />
+        </header>
+    );
+};
+
+export default Banner;
+
+// static images usually go on public folder
+// public folder is usually tha root folder, or the '/' folder
