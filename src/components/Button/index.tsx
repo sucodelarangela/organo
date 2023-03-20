8cdb7b3c4c13ef6015645525c25de725e26a3da4
Angela Caldas <aes.caldas@gmail.com>
2023-03-19 22:01:22 -0300

    refactor: change Button component from JSX to TSX

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,14 @@
+import { ReactElement } from 'react';
+import './style.css';
+
+interface ButtonProps {
+    children: ReactElement;
+}
+
+const Button = (props: ButtonProps) => {
+    return (
+        <button className='button'>{props.children}</button>
+    );
+};
+
+export default Button;
